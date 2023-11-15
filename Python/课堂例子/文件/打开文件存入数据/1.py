aef12e5f3fb7eca47d8b31c30bf9e1ac1a70dbdd
hewei2723 <hewei2723@qq.com>
2023-11-15 17:28:16 +0800

    无

--- Python/课堂例子/文件/打开文件存入数据/1.py
+++ Python/课堂例子/文件/打开文件存入数据/1.py
@@ -1,1 +1,15 @@
-fp=open("1.txt","r")
+a_list=[]
+while True:
+    s=input()
+    if s=="*":
+        break
+    a_list.append(s+"\n")
+
+fp=open(".//1.txt","a")
+fp.writelines(a_list)
+fp.close()
+fp=open(".//1.txt","r")
+str=fp.read()
+fp.close()
+print("输出文本文件内容：")
+print(str)
