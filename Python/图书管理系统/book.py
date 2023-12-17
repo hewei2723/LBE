1e1ce62c440854acfd8de8c32854e74044b023bd
hewei2723 <hewei2723@qq.com>
2023-12-17 22:25:55 +0800

    优化路径

--- Python/图书管理系统/book.py
+++ Python/图书管理系统/book.py
@@ -1,8 +1,8 @@
 import csv
 import os
-
+books_csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'books.csv')
 def list_books():
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'book.csv'), 'r') as f:
+    with open(books_csv_path, 'r') as f:
         reader = csv.reader(f)
         for row in reader:
             print(row)
@@ -12,7 +12,7 @@ def add_book():
     book_author = input("请输入作者：")
     book_quantity = input("请输入数量：")
 
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'books.csv'), 'a', newline='') as f:
+    with open(books_csv_path, 'a', newline='') as f:
         writer = csv.writer(f)
         writer.writerow([book_name, book_author, book_quantity])
 
@@ -20,11 +20,11 @@ def delete_book():
     book_name = input("请输入要删除的书名：")
 
     lines = []
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'books.csv'), 'r') as f:
+    with open(books_csv_path, 'r') as f:
         reader = csv.reader(f)
         lines = [row for row in reader if row[0] != book_name]
 
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'books.csv'), 'w', newline='') as f:
+    with open(books_csv_path, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(lines)
 
@@ -32,7 +32,7 @@ def update_book():
     book_name = input("请输入要更新的书名：")
 
     lines = []
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'books.csv'), 'r') as f:
+    with open(books_csv_path, 'r') as f:
         reader = csv.reader(f)
         lines = [row for row in reader if row[0] != book_name]
 
@@ -40,14 +40,14 @@ def update_book():
     book_quantity = input("请输入新的数量：")
     lines.append([book_name, book_author, book_quantity])
 
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'books.csv'), 'w', newline='') as f:
+    with open(books_csv_path, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(lines)
 def borrow_book():
     book_name = input("请输入要借阅的书名：")
 
     lines = []
-    with open('books.csv', 'r') as f:
+    with open(books_csv_path, 'r') as f:
         reader = csv.reader(f)
         lines = [row for row in reader]
 
@@ -60,7 +60,7 @@ def borrow_book():
                 print("这本书已经被借完了。")
             break
 
-    with open('books.csv', 'w', newline='') as f:
+    with open(books_csv_path, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(lines)
 
@@ -69,7 +69,7 @@ def return_book():
     book_name = input("请输入要归还的书名：")
 
     lines = []
-    with open('books.csv', 'r') as f:
+    with open(books_csv_path, 'r') as f:
         reader = csv.reader(f)
         lines = [row for row in reader]
 
@@ -79,6 +79,6 @@ def return_book():
             print("还书成功！")
             break
 
-    with open('books.csv', 'w', newline='') as f:
+    with open(books_csv_path, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(lines)
