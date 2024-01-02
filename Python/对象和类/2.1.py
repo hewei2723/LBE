3b96945616885d7dd96f52d81e38c399031598ce
hewei2723 <hewei2723@qq.com>
2024-01-03 00:34:38 +0800

    对象继承

---
+++ Python/对象和类/2.1.py
@@ -0,0 +1,34 @@
+class Animal:
+    def __init__(self, name, age):
+        # 姓名属性
+        self.name = name
+        # 年龄属性
+        self.age = age
+
+    # 吃饭行为
+    def eat(self):
+        print(f"{self.name} 正在吃东西")
+
+    # 睡觉行为
+    def sleep(self):
+        print(f"{self.name} 正在睡觉")
+
+class Dog(Animal):
+    # Dog类继承Animal类的属性和方法
+    pass
+
+class Cat(Animal):
+    # Cat类继承Animal类的属性和方法
+
+    # 在Cat类中创建新的方法
+    def play(self):
+        print(f"{self.name} 正在玩耍")
+
+if __name__ == "__main__":
+    # 创建Dog类的实例对象
+    my_dog = Dog("小汪", 4)
+    my_dog.eat()  # 调用基类中的方法
+
+    # 创建Cat类的实例对象
+    my_cat = Cat("小喵", 2)
+    my_cat.play()  # 调用Cat类中的方法
---
+++ Python/对象和类/3.py
@@ -0,0 +1,36 @@
+import math
+
+# 创建Point类
+class Point:
+    # 初始化函数
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+# 创建Line类
+class Line:
+    # 初始化函数
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    # 计算线段长度
+    def _calculate_length(self):
+        # 计算线段长度
+        return math.sqrt((self.point2.x - self.point1.x)**2 + (self.point2.y - self.point1.y)**2)
+
+    # 输出线段信息
+    def __str__(self):
+        # 输出属性的值
+        return f"线段端点1: ({self.point1.x}, {self.point1.y}), 线段端点2: ({self.point2.x}, {self.point2.y}), 线段长度: {self._calculate_length():.2f}"
+
+if __name__ == "__main__":
+    # 创建两个Point对象
+    point1 = Point(1, 2)
+    point2 = Point(4, 6)
+
+    # 创建Line对象
+    line = Line(point1, point2)
+
+    # 输出线段信息
+    print(line)
