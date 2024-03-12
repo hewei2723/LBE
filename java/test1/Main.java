e8395c94209e96e82e0bf34a591b20b8580af3c3
hewei2723 <hewei2723@qq.com>
2024-03-12 10:46:49 +0800

    html1

--- java/test1/Main.java
+++ java/test1/Main.java
@@ -2,15 +2,15 @@ public class Main{
     public static void main(String[] args){
         int[] arr = {1, 2, 3, 4, 5};
         int[] result = reverseArray(arr);
-        for(int i = 0; i < result.length; i++){
-            System.out.print(result[i] + " ");
+        for (int j : result) {
+            System.out.print(j + " ");
         }
     }
     public static int[] reverseArray(int[] arr){
         int[] result = new int[arr.length];
         for(int i = 0; i < arr.length; i++){
             result[i] = arr[arr.length - 1 - i];
-        }
+        }/Aaas
         return result;
     }
 }
