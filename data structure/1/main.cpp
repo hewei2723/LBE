64744e2ccc85cb89533ab60aea7409ffb215773a
hewei2723 <hewei2723@qq.com>
2024-03-28 16:46:14 +0800

    add c++

--- data structure/1/main.cpp
+++ data structure/1/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
+
 int main() {
-    int a, b, c;
-    std::cin >> a >> b >> c;
-    std::cout << a + b + c << std::endl; // 输出所有输入值的和
+    std::cout << "HELLOWORLD" << std::endl;
+    return 0;
 }
