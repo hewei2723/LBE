dad71394ac6ea106b11fb94f06a7d3608fd28720
hewei2723 <hewei2723@qq.com>
2024-03-28 15:48:03 +0800

    1

--- java/test1/Main.java
+++ java/test1/Main.java
@@ -10,7 +10,7 @@ public class Main{
         int[] result = new int[arr.length];
         for(int i = 0; i < arr.length; i++){
             result[i] = arr[arr.length - 1 - i];
-        }/Aaas
+        }
         return result;
     }
 }
