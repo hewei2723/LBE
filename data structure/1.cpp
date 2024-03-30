2daeb691a432c4343f7f8cede6a14c219927a891
hewei2723 <hewei2723@qq.com>
2024-03-30 10:00:50 +0800

    数据结构

--- data structure/1.cpp
+++ data structure/1.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
-
 using namespace std;
-
-// 定义顺序表的最大长度
-#define MAXSIZE 100
-
+// 定义顺序表的最大长度1
+#define MAXSIZE 5
 // 定义顺序表类型
 typedef int ElemType;
-
 // 定义顺序表结构
 struct SqList {
   ElemType data[MAXSIZE];
   int length;
 };
-
 // 初始化顺序表
 void InitList(SqList &L) { L.length = 0; }
-
 // 插入元素
 void InsertElem(SqList &L, int i, ElemType e) {
   if (i < 0 || i > L.length) return;
@@ -24,37 +18,29 @@ void InsertElem(SqList &L, int i, ElemType e) {
   L.data[i] = e;
   L.length++;
 }
-
 // 删除元素
 void DeleteElem(SqList &L, int i) {
   if (i < 0 || i >= L.length) return;
   for (int j = i + 1; j < L.length; j++) L.data[j - 1] = L.data[j];
   L.length--;
 }
-
 // 打印顺序表
 void PrintList(SqList L) {
   for (int i = 0; i < L.length; i++) cout << L.data[i] << " ";
   cout << endl;
 }
-
 int main() {
   SqList L;
   InitList(L);
-
   // 插入元素
   InsertElem(L, 0, 1);
   InsertElem(L, 1, 2);
   InsertElem(L, 2, 3);
-
   // 打印顺序表
   PrintList(L);
-
   // 删除元素
   DeleteElem(L, 1);
-
   // 打印顺序表
   PrintList(L);
-
   return 0;
 }
---
+++ data structure/单链表.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+// 定义节点
+struct Node {
+  int data;
+  Node *next;
+};
+// 定义头结点
+Node *head = NULL;
+// 插入节点
+void InsertNode(int data) {
+  Node *new_node = new Node;
+  new_node->data = data;
+  new_node->next = head;
+  head = new_node;
+}
+// 删除节点
+void DeleteNode(int data) {
+  Node *pre = NULL, *cur = head;
+  while (cur != NULL && cur->data != data) {
+    pre = cur;
+    cur = cur->next;
+  }
+  if (cur != NULL) {
+    if (pre == NULL) {
+      head = head->next;
+    } else {
+      pre->next = cur->next;
+    }
+    delete cur;
+  }
+}
+// 查找节点
+Node *FindNode(int data) {
+  Node *cur = head;
+  while (cur != NULL && cur->data != data) {
+    cur = cur->next;
+  }
+  return cur;
+}
+
+// 打印链表
+void PrintList() {
+  Node *cur = head;
+  while (cur != NULL) {
+    cout << cur->data << " ";
+    cur = cur->next;
+  }
+  cout << endl;
+}
+int main() {
+  // 插入节点
+  InsertNode(1);
+  InsertNode(2);
+  InsertNode(3);
+  // 打印链表
+  PrintList();
+  // 删除节点
+  DeleteNode(2);
+  // 打印链表
+  PrintList();
+  // 查找节点
+  Node *node = FindNode(3);
+  if (node != NULL) {
+    cout << "找到节点：" << node->data << endl;
+  } else {
+    cout << "未找到节点" << endl;
+  }
+  return 0;
+}
