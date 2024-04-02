f9ca12fb46b8fc279a49cb09342c0cd7241a52ff
hewei2723 <hewei2723@qq.com>
2024-04-02 17:42:22 +0800

    change

---
+++ java/面向对象编程/Main.java
@@ -0,0 +1,20 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+
+        a();
+
+
+
+    }
+    public static void a(){
+     int data=  b();
+        System.out.println("data:"+data);
+    }
+    public static int b(){
+        return 1;
+    }
+}
