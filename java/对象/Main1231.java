568c57f46910a038b7d29b68c1ad2b859aac0e83
hewei2723 <hewei2723@qq.com>
2024-04-04 15:47:08 +0800

    1

---
+++ java/对象/Main1231.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+        public static void main(String[] args){
+            Scanner x1=new Scanner(System.in);
+            System.out.println("Enter a number");
+            int num=x1.nextInt();
+            Scanner X2=new Scanner(System.in);
+            System.out.println("Enter a number");
+            int num2=X2.nextInt();
+            System.out.println("The sum of the numbers is "+(num+num2));
+        }
+    }
