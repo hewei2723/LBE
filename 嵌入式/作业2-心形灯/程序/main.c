c1a72eb6266a8287c7ce580b3e06ba10afbff568
hewei2723 <hewei2723@qq.com>
2024-09-24 01:01:42 +0800

    尝试使用指针，成功

--- 嵌入式/作业2-心形灯/程序/main.c
+++ 嵌入式/作业2-心形灯/程序/main.c
@@ -1,7 +1,20 @@
-#include<reg51.h>
-int led[3][7];//三个组，每个组有7个接口
-int io={};//这个用来存放电平,就八个模式
-int main(){
-	
+#include <reg51.h>
+int led[8] = {0Xfe, 0xfd, 0xfb, 0xf7, 0xef, 0xdf, 0xbf, 0x7f};
+void delay(unsigned int x){
+	while(x--);
 }
-	
+void main() {
+    int *l;
+    l = &led[0];
+    while (l < &led[8]) { 
+        P0 = *l;
+						delay(5000);
+				P1=*l;
+						delay(5000);
+			P2=*l;
+						delay(5000);
+			P3=*l;
+			delay(5000);
+        l++;
+    }
+}
