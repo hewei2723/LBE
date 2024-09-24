8232b48e6f01cd87c570a4118c18bd4725dc6cf7
hewei2723 <hewei2723@qq.com>
2024-09-24 21:20:47 +0800

    心灯做完了

--- 嵌入式/作业2-心形灯/程序/main.c
+++ 嵌入式/作业2-心形灯/程序/main.c
@@ -1,20 +1,101 @@
 #include <reg51.h>
-int led[8] = {0Xfe, 0xfd, 0xfb, 0xf7, 0xef, 0xdf, 0xbf, 0x7f};
-void delay(unsigned int x){
-	while(x--);
+int led1[9] = {0Xfe, 0xfd, 0xfb, 0xf7, 0xef, 0xdf, 0xbf, 0x7f, 0xff};
+int led2[8] = {0Xfc, 0xf9, 0xf3, 0xe7, 0xcf, 0x9f, 0x3f, 0xff};
+int i, j;
+void delay(unsigned int x)
+{
+	while (x--)
+		;
 }
-void main() {
-    int *l;
-    l = &led[0];
-    while (l < &led[8]) { 
-        P0 = *l;
-						delay(5000);
-				P1=*l;
-						delay(5000);
-			P2=*l;
-						delay(5000);
-			P3=*l;
-			delay(5000);
-        l++;
-    }
+one()
+{
+	for (i = 0; i <= 3; i++)
+	{
+		switch (i)
+		{
+		case 0:
+			for (j = 0; j < 8; j++)
+			{
+				P0 = led1[j];
+				delay(5000);
+				P0 = led1[8];
+			}
+			P0 = led1[8];
+			break;
+		case 1:
+			for (j = 0; j < 8; j++)
+			{
+				P1 = led1[j];
+				delay(5000);
+				P1 = led1[8];
+			}
+			break;
+		case 2:
+			for (j = 0; j < 8; j++)
+			{
+				P2 = led1[j];
+				delay(5000);
+				P2 = led1[8];
+			}
+			break;
+		case 3:
+			for (j = 0; j < 8; j++)
+			{
+				P3 = led1[j];
+				delay(5000);
+				P3 = led1[8];
+			}
+			break;
+		}
+	}
+}
+two()
+{
+	for (i = 0; i <= 3; i++)
+	{
+		switch (i)
+		{
+		case 0:
+			for (j = 0; j < 8; j++)
+			{
+				P0 = led2[j];
+				delay(5000);
+				P0 = led2[7];
+			}
+			P0 = led2[7];
+			break;
+		case 1:
+			for (j = 0; j < 7; j++)
+			{
+				P1 = led2[j];
+				delay(5000);
+				P1 = led2[7];
+			}
+			P1 = led2[7];
+			break;
+		case 2:
+			for (j = 0; j < 7; j++)
+			{
+				P2 = led2[j];
+				delay(5000);
+				P2 = led2[7];
+			}
+			P2 = led2[7];
+			break;
+		case 3:
+			for (j = 0; j < 7; j++)
+			{
+				P3 = led2[j];
+				delay(5000);
+				P3 = led2[7];
+			}
+			P3 = led2[7];
+			break;
+		}
+	}
+}
+void main()
+{
+	one();
+	two();
 }
