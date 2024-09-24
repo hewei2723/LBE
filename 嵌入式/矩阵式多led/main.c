5ec5284812bcc3374b3350b09e35a09d54c8b2b9
hewei2723 <hewei2723@qq.com>
2024-09-24 23:24:18 +0800

    矩阵驱动完成

--- 嵌入式/矩阵式多led/main.c
+++ 嵌入式/矩阵式多led/main.c
@@ -1,23 +1,66 @@
-#include<reg51.h>
-sbit x1=P0^0;
-sbit x2=P0^1;
-sbit x3=P0^2;
-sbit x4=P0^3;
-sbit x5=P0^4;
-sbit x6=P0^5;
-//Y
-sbit y1=P2^0;
-sbit y2=P2^1;
-sbit y3=P2^2;
-sbit y4=P2^3;
-sbit y5=P2^4;
-sbit y6=P2^5;
+
+#include <reg51.h>
+sbit x1 = P0^0;
+sbit x2 = P0^1;
+sbit x3 = P0^2;
+sbit x4 = P0^3;
+sbit x5 = P0^4;
+sbit x6 = P0^5;
+// Y
+sbit y1 = P2^0;
+sbit y2 = P2^1;
+sbit y3 = P2^2;
+sbit y4 = P2^3;
+sbit y5 = P2^4;
+sbit y6 = P2^5;
 
 void delay(unsigned int t)
 {
     while (t--);
 }
-void main(){
-	x1=0;
-	y1=1;
+//当y为高 x为低时，灯亮
+void turn_on_led(int row, int col) {
+    x1 = x2 = x3 = x4 = x5 = x6 = 1;
+    y1 = y2 = y3 = y4 = y5 = y6 = 0;
+    switch(row) {
+        case 1: x1 = 0; break;
+        case 2: x2 = 0; break;
+        case 3: x3 = 0; break;
+        case 4: x4 = 0; break;
+        case 5: x5 = 0; break;
+        case 6: x6 = 0; break;
+    }
+    switch(col) {
+        case 1: y1 = 1; break;
+        case 2: y2 = 1; break;
+        case 3: y3 = 1; break;
+        case 4: y4 = 1; break;
+        case 5: y5 = 1; break;
+        case 6: y6 = 1; break;
+    }
+}
+//当x=y时不亮
+void turn_off_led(int row, int col) {
+    switch(row) {
+        case 1: x1 = 1; break;
+        case 2: x2 = 1; break;
+        case 3: x3 = 1; break;
+        case 4: x4 = 1; break;
+        case 5: x5 = 1; break;
+        case 6: x6 = 1; break;
+    }
+    switch(col) {
+        case 1: y1 = 0; break;
+        case 2: y2 = 0; break;
+        case 3: y3 = 0; break;
+        case 4: y4 = 0; break;
+        case 5: y5 = 0; break;
+    }
+}
+void main() {
+    while (1) {
+        turn_on_led(1, 2);  // Turn on the LED at (x1, y1)
+        delay(1000);       // Wait for a while
+        turn_off_led(1, 2); // Turn off the LED at (x1, y1)
+    }
 }
