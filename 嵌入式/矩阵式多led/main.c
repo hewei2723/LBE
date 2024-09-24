26d85a0ac3fa3f067f367567d21ca308ad3c777e
hewei2723 <hewei2723@qq.com>
2024-09-25 00:08:42 +0800

    单个控制

--- 嵌入式/矩阵式多led/main.c
+++ 嵌入式/矩阵式多led/main.c
@@ -13,18 +13,43 @@ sbit y3 = P2^2;
 sbit y4 = P2^3;
 sbit y5 = P2^4;
 sbit y6 = P2^5;
+int result[18][2];  // 6对 + 6对 + 6对，总共18个(x, y)对
 //这里放字模
 //I x[1-6]y[6]
 //I x[3]y[1-6]
 //I x[1-6]y[1]
+int I() {
+		int x,y;
+    // 定义数组存储符合条件的(x, y)对
+    int index = 0;
+    // I x[1-6] y[6]
+    for ( x = 1; x <= 6; x++) {
+        result[index][0] = x;
+        result[index][1] = 6;
+        index++;
+    }
+    // I x[3] y[1-6]
+    for ( y = 1; y <= 6; y++) {
+        result[index][0] = 3;
+        result[index][1] = y;
+        index++;
+    }
+    // I x[1-6] y[1]
+    for ( x = 1; x <= 6; x++) {
+        result[index][0] = x;
+        result[index][1] = 1;
+        index++;
+    }
+}
+
 void delay(unsigned int t)
 {
     while (t--);
 }
 //当y为高 x为低时，灯亮
 void turn_on_led(int row, int col) {
-    x1 = x2 = x3 = x4 = x5 = x6 = 1;
-    y1 = y2 = y3 = y4 = y5 = y6 = 0;
+    int x1 = x2 = x3 = x4 = x5 = x6 = 1;
+    int y1 = y2 = y3 = y4 = y5 = y6 = 0;
     switch(row) {
         case 1: x1 = 0; break;
         case 2: x2 = 0; break;
@@ -61,9 +86,14 @@ void turn_off_led(int row, int col) {
     }
 }
 void main() {
+	int i;
     while (1) {
-        turn_on_led(1, 2);  // Turn on the LED at (x1, y1)
-        delay(1000);       // Wait for a while
-        turn_off_led(1, 2); // Turn off the LED at (x1, y1)
+        I();
+        for ( i = 0; i <18; i++)
+        {
+             turn_on_led(result[i][0], result[i][1]);
+        }
+        
+       
     }
 }
