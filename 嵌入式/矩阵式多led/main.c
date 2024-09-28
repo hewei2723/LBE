d7e5b50081d53fd9467a7073ea53bf3ba66fe8c1
hewei2723 <hewei2723@qq.com>
2024-09-28 10:43:15 +0800

    rebuild

--- 嵌入式/矩阵式多led/main.c
+++ 嵌入式/矩阵式多led/main.c
@@ -1,103 +1,16 @@
 #include <reg51.h>
-sbit x1 = P0^0;
-sbit x2 = P0^1;
-sbit x3 = P0^2;
-sbit x4 = P0^3;
-sbit x5 = P0^4;
-sbit x6 = P0^5;
-// Y
-sbit y1 = P2^0;
-sbit y2 = P2^1;
-sbit y3 = P2^2;
-sbit y4 = P2^3;
-sbit y5 = P2^4;
-sbit y6 = P2^5;
-void delay(int x){
-while(x--);}
-int result[18][2]; 
-void I() {
-    int index = 0;
-    int x, y;
-
-    // I x[1-6] y[6]
-    for (x = 1; x <= 6; x++) {
-        result[index][0] = x;
-        result[index][1] = 6;
-        index++;
-    }
-
-    // I x[3] y[1-6]
-    for (y = 1; y <= 6; y++) {
-        result[index][0] = 3;
-        result[index][1] = y;
-        index++;
-    }
-
-    // I x[1-6] y[1]
-    for (x = 1; x <= 6; x++) {
-        result[index][0] = x;
-        result[index][1] = 1;
-        index++;
-    }
+int x[8]={0x00,0x1c,0x08,0x08,0x08,0x1c,0x00,0x00};
+int y[8]={0x80,0x40,0x20,0x10,0x08,0x04,0x02,0x01};
+void delay(unsigned int t) {
+    while (t--);
 }
-
-void led_on(int x,int y){
-	y1=y2=y3=y4=y5=y6=0;
-	x1=x2=x3=x4=x5=x6=1;
-	switch (x)
-    {
-    case 1:
-        x1=0;
-        break;
-    case 2:
-        x2=0;
-        break;
-    case 3:
-        x3=0;
-        break;
-    case 4:
-        x4=0;
-        break;
-    case 5:
-        x5=0;
-        break;
-    case 6:
-        x6=0;
-        break;
-    
-    default:
-        break;
-    }
-    switch (y)
-    {
-    case 1:
-        y1=1;
-        break;
-    case 2:
-        y2=1;
-        break;
-    case 3:
-        y3=1;
-        break;
-    case 4:
-        y4=1;
-        break;
-    case 5:
-        y5=1;
-        break;
-    case 6:
-        y6=1;
-    }
-}
-int main() {
+void main() {
     int i;
-    while (1) {
-        I();
-        for (i = 0; i < 18; i++) {
-            led_on(result[i][0], result[i][1]);
-        }
-									delay(900);
-    }
-
-    return 0;
+    // for (i=0;i<8;i++) {
+    //     delay(2000);
+    //     P2=x[i];
+    //     P0=y[i];
+    //     }
+	P2=0XFE;
+	P0=0XFF;
 }
