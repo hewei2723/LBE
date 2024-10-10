4869a753c912b8b93f26eb45668501c600e63857
hewei2723 <hewei2723@qq.com>
2024-10-10 15:48:57 +0800

    B2020,但是oj跑不了，自己的是对的

--- LuoGU/B2020/main.c
+++ LuoGU/B2020/main.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
-int main(){
-    int a[5]={8,9,10,11,12};
-    int b[5];//用来存储余数，剩下的苹果
+int main()
+{
+    int a[5] = {};
+    int b=0; // 用来存储余数，剩下的苹果
+    scanf("%d %d %d %d %d", &a[0], &a[1], &a[2], &a[3], &a[4]);
     for (int i = 0; i < 5; i++)
     {
-        a[i+1]=a[i+1]+a[i]/3;
-        a[4-i]=a[4-i]+a[i]/3;
-        a[i]=a[i]-a[i]/3;
+        if (i == 0)
+        {
+            a[4] = a[4] + a[0] / 3;
+            a[1] = a[1] + a[0] / 3;
+            b= a[0] % 3;
+        }
+        else
+        {
+            a[i + 1] = a[i + 1] + a[i] / 3;
+            a[i - 1] = a[i - 1] + a[i] / 3;
+            b=b+ a[i] % 3;
+            a[i] = a[i] / 3;
+        }
     }
     for (int i = 0; i < 5; i++)
     {
-        printf("%d ",a[i]);
+        printf("%d ", a[i]);
     }
+    printf("\n%d",b);   
 }
