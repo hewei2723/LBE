440392d7802ff65035ffbaf702979f9aba35e804
hewei2723 <hewei2723@qq.com>
2024-10-11 09:37:50 +0800

    按键

--- 嵌入式/按钮/程序/key.c
+++ 嵌入式/按钮/程序/key.c
@@ -1,1 +1,16 @@
-#include "r"
+#include "reg51.h"
+sbit key = P0^7;
+sbit led= P0^0;
+void delay(unsigned int t){
+    while (x--);
+}
+int main(void){
+    if key == 0{
+        delay(200);
+        while (key==0)
+        {              
+        led = 1;
+        }
+
+    }
+}
