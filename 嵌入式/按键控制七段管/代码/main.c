98b5ce9ecdbbc636d1be7bad567b902e31f89a31
hewei2723 <hewei2723@qq.com>
2024-10-13 20:16:11 +0800

    做好啦！

--- 嵌入式/按键控制七段管/代码/main.c
+++ 嵌入式/按键控制七段管/代码/main.c
@@ -1,12 +1,13 @@
 #include <reg51.h>
 int gnd[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
-sbit key4=P0^7;
-sbit key3=P2^7;
-sbit key3=P3^7;
-sbit key3=P1^7;
+sbit key4 = P0 ^ 7;
+sbit key3 = P2 ^ 7;
+sbit key2 = P3 ^ 7;
+sbit key1 = P1 ^ 7;
 void delay(unsigned int x)
 {
-    while (x--);
+    while (x--)
+        ;
 }
 void show(unsigned int num)
 {
@@ -23,8 +24,34 @@ void show(unsigned int num)
 }
 void main()
 {
-    unsigned int i;
-    for (i = 0; i <= 9999; i++){
+    int i = 611;
+    while (1)
+    {
         show(i);
-		}
-}
+        if (key1 == 0)
+        {
+            delay(13000);
+            while (key1 == 0)
+            {
+                show(i = i + 1);
+            }
+        }
+         if (key2 == 0)
+        {
+            delay(14000);
+            while (key2 == 0)
+            {
+                show(i = i + 10);
+            }
+        }
+		if (key3 == 0)
+        {
+            delay(14000);
+            while (key3 == 0)
+            {
+			while(i<779){
+			show(i++);}
+			}
+            }
+        }
+    }
