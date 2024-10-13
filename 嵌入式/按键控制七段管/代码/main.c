4245105a6fe0649ccf159cb678e40cb5c2114227
hewei2723 <hewei2723@qq.com>
2024-10-13 19:32:25 +0800

    用好看的图

---
+++ 嵌入式/按键控制七段管/代码/main.c
@@ -0,0 +1,26 @@
+#include <reg51.h>
+int gnd[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
+void delay(unsigned int x)
+{
+    while (x--);
+}
+void show(unsigned int num)
+{
+    int a, b, c, d;
+    d = num / 1000;
+    c = (num % 1000) / 100;
+    b = (num % 100) / 10;
+    a = num % 10;
+    P0 = gnd[d];
+    P2 = gnd[c];
+    P3 = gnd[b];
+    P1 = gnd[a];
+    delay(5000);
+}
+void main()
+{
+    unsigned int i;
+    for (i = 0; i <= 9999; i++){
+        show(i);
+		}
+}
