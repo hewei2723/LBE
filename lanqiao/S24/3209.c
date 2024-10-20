70bd81a04ec6e6d3937706c9687ac6849695cf95
hewei2723 <hewei2723@qq.com>
2024-10-20 15:24:50 +0800

    爬山题目，测试项是对的，没给数据范围，可以骗分

--- lanqiao/S24/3209.c
+++ lanqiao/S24/3209.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
 #include <math.h>
 int n[7];
 int length(int n) // 获取长度
@@ -56,40 +55,3 @@ int main()
     }
     printf("%d", sum);
 }
-=======
-
-// 函数：判断一个数是否为“好数”
-int isGoodNumber(int num) {
-    int position = 1; // 从个位开始，位置从 1 开始
-    while (num > 0) {
-        int digit = num % 10;
-        if (position % 2 == 1) { // 奇数位
-            if (digit % 2 == 0) {
-                return 0; // 不满足条件
-            }
-        } else { // 偶数位
-            if (digit % 2 != 0) {
-                return 0; // 不满足条件
-            }
-        }
-        num /= 10;
-        position++;
-    }
-    return 1; // 满足所有位的条件
-}
-
-int main() {
-    int N;
-    scanf("%d", &N);
-    
-    int count = 0;
-    for (int num = 1; num <= N; num++) {
-        if (isGoodNumber(num)) {
-            count++;
-        }
-    }
-    
-    printf("%d", count);
-    return 0;
-}
->>>>>>> fe2b4c9a2025de515b12ec0f3e2365ab7bd43e09
