aa346ab5ba2830f3c4d27d863b46b43e2e1f6878
hewei2723 <hewei2723@qq.com>
2024-10-25 17:23:50 +0800

    还差时间显示

--- 嵌入式/项目6/代码/main.c
+++ 嵌入式/项目6/代码/main.c
@@ -1,7 +1,10 @@
 #include <reg51.h>
 int seg[10] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
 int ptwo[8] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};
-int i=0,hours,minutes,second;
+int i=0,hours,minutes,second,cp;
+struct tim{
+	int a,b;
+}tim;
 void delay(int x)
 {
 	while (x--)
@@ -14,26 +17,35 @@ void timer_init(void)
 	TH0 = (65535 - 2000) / 256;
 	EA = 1;
 	ET0 = 1;
-}//����
+}
+struct tim nu(int x) {
+    struct tim numm;
+    numm.a = x / 10;
+    numm.b = x % 10;
+    return numm;
+}
 void timer0_isr(void) interrupt 1
 {
 	TL0 = 0X78;
 	TH0 = 0Xec;
 	cp++;
-	if (cp = > 200)
+	if (cp > 200)
 	{
 		cp = 0;
 		i++;
 		if(i==60*60*24){
 			i=0;
-		}//����24Сʱ����
+		}
 	}
 	hours=i/3600;
 	minutes=(i%3600)/60;
 	second=i%60;
-	//���֮��i�Ƕ��پ͹��˶�����
-	//�������ʾʱ��Ĵ���
-	
+	//这里应该是显示小时那段的代码
+	nu(hours);
+	//这里应该是显示分钟那段的代码
+	nu(minutes);
+	//这里应该是显示秒那段的代码
+	nu(second);
 }
 int showStu()
 {
