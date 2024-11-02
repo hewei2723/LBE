e042f8ab9065dc712cd207e47916fef0c1f14dd6
hewei2723 <hewei2723@qq.com>
2024-11-02 22:51:57 +0800

    爬山

--- C/1.cpp
+++ C/1.cpp
@@ -1,9 +1,35 @@
-#include <stdio.h>
-int main(){
-int num=1234;
-int thousand, ten, hundred, unit;
-thousand = num / 1000;
-hundred = (num % 1000) / 100;
-ten = (num % 100) / 10;
-unit = num % 10;
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cmath>
+using namespace std;
+int main() {
+    int n,p,q,power=0,eneger=0;
+    cin>>n>>p>>q;
+    vector<int> a(n);
+    priority_queue<int> pw;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        pw.push(a[i]);//将数字推入堆中
+        }
+    for (int i = 0; i < p; i++)
+    {
+        power=pw.top();//取出堆顶元素
+        pw.pop();//删除堆顶元素
+        pw.push(sqrt(power));//将堆顶元素开平方后推入堆中后推入堆中
+        //p次操作后完成削弱
+    }
+    for (int i = 0; i < q; i++)
+    {
+        /* code */
+        power=pw.top();//取出堆顶元素
+        pw.pop();//删除堆顶元素
+        pw.push(power*0.5);//将堆顶元素乘2后推入堆中
+        //q次操作后完成削弱
+    }
+    while(!pw.empty()){
+        eneger+=pw.top();
+        pw.pop(); 
+    }
+    cout<<eneger<<endl;
 }
