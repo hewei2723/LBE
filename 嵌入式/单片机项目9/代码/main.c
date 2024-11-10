58aa67f0d0e6391c0cdbc71d14368302cf18b1bc
hewei2723 <hewei2723@qq.com>
2024-11-10 16:49:20 +0800

    消抖完成

--- 嵌入式/单片机项目9/代码/main.c
+++ 嵌入式/单片机项目9/代码/main.c
@@ -8,6 +8,8 @@ int cp = 0, cpfh = 0;time = 0, flash = 0x00, start = 1, mode = 0;
 int secondA, hourA, minuteA, secondB, minuteB, hourB;
 int poi[] = {0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01};
 unsigned char seven_seg[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
+int sno[]={1,1,6,0,1,0,2,2};
+void key();
 void latch()
 {
 	LE = 1;
@@ -26,23 +28,32 @@ void STM(int time)
 	hourA = (time / 3600) / 10;
 	hourB = (time / 3600) % 10;
 }
+void showSno(){
+	int i;
+	for(i=0;i<8;i++){
+	P0 = 0xff;P0 = poi[i];latch();P0 = seven_seg[sno[i]];delay(300);
+	}
+}
 void timer0_isr() interrupt 1
 {
 	TH0 = (65535 - 5000) / 256;
 	TL0 = (65535 - 5000) % 256;
 	cp++;
 	cpfh++;
-	if (cp > 100 && start == 1)
+	if (cp > 25 && start == 1)
 	{
 		time++;
 		cp = 0;
 	}
-	if (cpfh > 100)
+	if (cpfh > 25)
 	{
 		flash = ~flash;
 		cpfh = 0;
 	}
 	STM(time);
+	if(time<1){
+	showSno();}else{key();}
+
 }
 void timer0_init()
 {
@@ -110,7 +121,7 @@ void ShowTime(int num, int mode)//mode=0 ++ mode=1 +60 mode =2 +3600
 void changeTime()
 {
 	int s;
-	int keyS2 = 1, keyS3 = 1; //
+	static int keyS2 = 1, keyS3 = 1; // 按键状态
 	switch (mode)
 	{
 	case 0:
@@ -123,27 +134,34 @@ void changeTime()
 		s = 3600;
 		break;
 	}
-	if (keyS2 == 1 && key2 == 0)
+	if (keyS2 == 1 && key2 == 0)  // 按键2按下并检测状态
 	{
-		delay(50);
-		if (key2 == 0)
+		delay(200);  // 消抖延时
+		if (key2 == 0) // 确认按键仍然按下
 		{
 			time += s;
+			keyS2 = 0;  // 置为0，防止重复触发
 		}
-		keyS2 = key2;
 	}
-	if (keyS3 == 1 && key3 == 0)
+	else if (key2 == 1) // 按键松开后重置状态
 	{
-		delay(50);
-		if (key3 == 0)
+		keyS2 = 1;
+	}
+	if (keyS3 == 1 && key3 == 0) // 按键3按下并检测状态
+	{
+		delay(200);  // 消抖延时
+		if (key3 == 0) // 确认按键仍然按下
 		{
 			time -= s;
+			keyS3 = 0;  // 置为0，防止重复触发
 		}
-		keyS3 = key3;
 	}
-	delay(1500);//随后的1500内都不响应
-	keyS2=keyS3=1;
+	else if (key3 == 1) // 按键松开后重置状态
+	{
+		keyS3 = 1;
+	}
 }
+
 void key(){
 	int keyL=1;
 	if(start){
@@ -160,7 +178,7 @@ void key(){
 		mode=(mode+1)%3;//按下一次mode值变化一次，从0，1，2循环变化
 		}
 		keyL=key1;
-		delay(50);
+		delay(100);
 	}
 	keyL=1;
 	if(!start){//如果当前是暂停状态
@@ -193,8 +211,5 @@ void key(){
 void main()
 {
 	timer0_init();
-	while (1)
-	{
-		key();
-	}
+	while (1);
 }
