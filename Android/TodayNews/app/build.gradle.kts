b3bdaa9b1e16c56a0d449a867c07184a8624b055
hewei2723 <hewei2723@qq.com>
2024-12-19 22:10:49 +0800

    这个是安卓vela同步软件

--- Android/TodayNews/app/build.gradle.kts
+++ Android/TodayNews/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 1
-        versionName = "1.5.2"
+        versionName = "1.5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
