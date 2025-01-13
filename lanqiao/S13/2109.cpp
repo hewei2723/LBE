c2d3a3c4f7e5e75230c93958898789fdc2367269
hewei2723 <hewei2723@qq.com>
2025-01-13 19:52:15 +0800

    清理

--- lanqiao/S13/2109.cpp
+++ lanqiao/S13/2109.cpp
@@ -1,14 +1,12 @@
 #include<iostream>
 using namespace std;
-int n=3,m=4,k=10;
-int main(){
-    // cin>>n>>m>>k;
-    int t[n][m]={1,2,3,4,5,6,7,8,9,10,11,12};
-    //遍历矩阵从0到n,从0到m
-    for(int x=0;x<n;x++){
-        for(int y=0;y<m;y++){
-            cout<<t[x][y]<<" ";
+int main() {
+    const int n = 3, m = 4, k = 10;  // 使用常量
+    int t[n][m] = {1, 2, 3, 4,5, 6, 7, 8,9, 10, 11, 12};  // 初始化二维数组
+    // 遍历矩阵从0到n, 从0到m
+    for (int x = 0; x < n; x++) {
+        for (int y = 0; y < m; y++) {
+            
         }
-        cout<<"\n";
     }
-    }
+}
