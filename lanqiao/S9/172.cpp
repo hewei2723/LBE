a2fa4ae444983d64124ca70a361677fefd58fe6a
hewei2723 <hewei2723@qq.com>
2025-03-07 17:41:51 +0800

    排序

--- lanqiao/S9/172.cpp
+++ lanqiao/S9/172.cpp
@@ -1,6 +1,30 @@
-#include <iostream>
+/*
+找出b[i]>a[i]的次数
+找出b[i]<c[i]的次数
+排序a,c
+*/
+#include <bits/stdc++.h>
 using namespace std;
+int find(int a){
+    
+}
 int main()
 {
-  return 0;
+    int n = 3, ct, num, a[n], b[n], c[n];
+    for (int i = 0; i < 3; i++)
+    {
+        cin >> a[i];
+    };
+    sort(a, a + n);
+    for (int i = 0; i < 3; i++)
+    {
+        cin >> b[i];
+    }
+    sort(b, b + n);
+    for (int i = 0; i < 3; i++)
+    {
+        cin >> c[i];
+    }
+    sort(c, c + n);
+    return 0;
 }
