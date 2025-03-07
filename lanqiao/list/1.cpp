c4fa247725e646a2ed361f33729e79a260ae6c8a
hewei2723 <hewei2723@qq.com>
2025-03-07 17:57:50 +0800

    排序

--- lanqiao/list/1.cpp
+++ lanqiao/list/1.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+int num,n;
+vector<int> nums,storg;
 int main()
 {
-    
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>num;
+        nums.push_back(num);
+    };
+    sort(nums.begin(),nums.end());
+    for(int nn:nums){
+        cout<<nn;
+    }
+    for(int i=nums.size();i>=0;i--){
+        cout<<nums[i];
+    }
     return 0;
 }
